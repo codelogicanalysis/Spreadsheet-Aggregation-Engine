838f1915f96be741723fdb2af9918dfe55ba082f
Elie Maamary <eliemaamary17@gmail.com>
2018-02-20 14:58:57 +0200

    Update parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,15 +5,13 @@ std::vector<std::string> Parser::parse_csv_line(std::string line) {
 	std::stringstream lineStream(line);
 	std::string cell;
 
-	while(std::getline(lineStream,cell, ','))
-	{
+	while(std::getline(lineStream,cell, ',')) {
 		// Remove any whitespaces in values or column names, then delete extra character
 		cell.erase(remove_if(cell.begin(), cell.end(), isspace), cell.end()); 
 		result.push_back(cell);
 	}
     	// This checks for a trailing comma with no data after it.
-    	if (!lineStream && cell.empty())
-    	{
+    	if (!lineStream && cell.empty()) {
         	// If there was a trailing comma then add an empty element.
         	result.push_back("");
 	}
