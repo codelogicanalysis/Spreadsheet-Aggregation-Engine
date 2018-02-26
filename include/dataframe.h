9a1da973c5a1bef3dabbd419e5ab618d4c8c6f43
fadi zaraket <fadizaraket@gmail.com>
2018-02-26 19:43:35 +0200

    Update dataframe.h

--- include/dataframe.h
+++ include/dataframe.h
@@ -5,15 +5,19 @@
 #include <string>
 #include "parser.h"
 
+typedef std::unordered_map< std::string, std::vector<std::string> > Str2VecStrMap_t; 
+ 
 class DataFrame {
 private:
-	std::unordered_map< std::string, std::vector<std::string> > data;	
+	String2VectorOfStringsMap_t data;	
 	std::vector<std::string> header;
 	int column_count, row_count;
 
 public:
 	DataFrame(std::string filename);
 	std::vector<std::string> column_names();
+	//FZ: avoid copying by using references
+	//FZ: if you care preserving the original data: use const 
 	std::vector< std::pair<std::string, std::string> > row_at_index(int row_index);
 	std::vector<std::string> get_column(std::string column_name);
 	std::vector<std::string> column_types();
